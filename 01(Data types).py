764fabdeaf1c49a7644176442183e88ac2bce6c7
girishgirii <143625117+girishgirii@users.noreply.github.com>
2023-12-11 07:22:26 +0530

    Add files via upload

---
+++ 01(Data types).py
@@ -0,0 +1,37 @@
+number1=100
+print("The type of number 1",type(number1))
+integer_1=100
+integer_2=50
+print(integer_1*integer_2)
+print(integer_1+integer_2)
+print(integer_1-integer_2)
+print(integer_1/integer_2)
+number2=100.0
+print("The type of number 2",type(number2))
+float_1=12.539
+float_2=6.78
+print(float_1*float_2)
+print(float_1+float_2)
+print(float_1-float_2)
+print(float_1/float_2)
+str="Jack denceil"
+print("The type of str",type(str))
+String_1="Hello"
+String_2="World"
+print(String_1+String_2)
+result=100<200
+print("The type of result",type(result))
+has_passed=False
+marks=80
+if(marks>50):
+    has_passed=True
+    
+    
+print("list")
+L1=[1,2,"ABC",3,"XYZ",2,3]
+print(L1)
+print("tuples")
+T1=(10,20,30,40,50)
+print(T1)
+print("sets")
+S1={10,30,20,40,10,30,40,20,50,50}
